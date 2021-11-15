707873412b2959d82d1f331376c9b28fa9d04bd4
Daniel <danielh.analuisa@gmail.com>
2021-11-15 13:23:22 -0500

    Agregar archivos de proyecto.

---
+++ App5/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using Xamarin.Forms.Xaml;
+
+
+namespace App5
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://10.1.60.2/moviles/post.php";
+        private readonly HttpClient Client = new HttpClient();
+        private ObservableCollection<App5.Datos> _post;
+        public MainPage()
+        {
+            InitializeComponent();
+            get();
+            int edad;
+            
+        }
+
+        public async void get()
+        {
+            try
+            {
+                var content = await Client.GetStringAsync(Url);
+                List<App5.Datos> posts = JsonConvert.DeserializeObject<List<App5.Datos>>(content);
+                _post = new ObservableCollection<App5.Datos>(posts);
+                MyListView.ItemsSource = _post;
+
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "ERROR " + ex.Message, "OK");
+            }
+
+        }
+
+       
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            var dt = menuItem.CommandParameter as App5.Datos;
+
+            await Navigation.PushAsync(new actualizar(dt.codigo, dt.nombre, dt.apellido, dt.edad));
+        }
+
+        private async void MenuItem_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
---
+++ App5/actualizar.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class actualizar : ContentPage
+    {
+        public actualizar(int codigo, string nombre, string apellido, int edad)
+        {
+            InitializeComponent();
+            txtCodigo.Text = codigo.ToString();
+            txtNombre.Text = nombre;
+            txtApellido.Text = apellido;
+            txtEdad.Text = edad.ToString();
+        }
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnGuardar_Clicked_2(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                //Actualizar con PUT
+                var response = client.PutAsync(String.Format("http://10.1.60.2/moviles/post.php?codigo={0}&nombre={1}&apellido={2}&edad={3}", txtCodigo.Text, txtNombre.Text, txtApellido.Text, txtEdad.Text), null).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var resultString = response.Content.ReadAsStringAsync().Result;
+                }
+                else
+                {
+                    await DisplayAlert("Mensaje de alerta", "Error al actualizar", "OK");
+                }
+
+                // Utilizar TOAST
+                /*var mensaje = "Actualizado correctamente";
+                DependencyService.Get<Mensaje>().longAlert(mensaje);*/
+
+                //Regresar a la pantalla de Vacunas
+                await Navigation.PushAsync(new MainPage());
+
+                // Poner en blanco los campos
+                txtNombre.Text = "";
+                txtApellido.Text = "";
+                txtEdad.Text = "";
+                await DisplayAlert("Mensaje", "aztualizado correctamente", "OK");
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Mensaje de alerta", ex.Message, "OK");
+            }
+        }
+    }
+}
